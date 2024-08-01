5c5f46748aff9d6ab9f65e0645d98b1fea6295aa
smirnovadev <annaparfenova514@gmail.com>
2024-08-01 23:49:09 +0300

    настройка build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -50,7 +51,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
     implementation("androidx.activity:activity-compose:1.9.1")
@@ -59,6 +59,34 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.room:room-runtime:2.5.2")
+    kapt("androidx.room:room-compiler:2.5.2")
+    implementation("androidx.room:room-ktx:2.5.2")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.3")
+
+    // Livedata
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.3")
+
+    // Dagger Core
+    implementation("com.google.dagger:dagger:2.44.2")
+    implementation("com.google.dagger:dagger-android:2.44.2")
+    implementation("com.google.dagger:dagger-android-support:2.44.2")
+
+    // Annotation processor
+    kapt("com.google.dagger:dagger-compiler:2.44.2")
+    kapt("com.google.dagger:dagger-android-processor:2.44.2")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.14.2")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
